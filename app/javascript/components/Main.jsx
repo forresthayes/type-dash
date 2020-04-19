3eb44f50581f174681d501f0158d62dd3b935c96
Forrest Hayes <forresthayes@pm.me>
2020-04-19 16:14:28 -0500

    Make typing prompt cycle through Sonnet 29 couplets

--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -1,26 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { testPrompt } from '../helpers'
 import 'bulma/css/bulma.css'
 import WarningModal from './WarningModal'
 import {
-  Button,
   Container,
   Column,
   Columns,
   Box,
   Field,
-  Section
+  Section,
 } from 'bloomer'
 
+
 export default () => {
-  const START_TIME = 200
+  const [promptGen, setPromptGen] = useState(testPrompt())
+  const [prompt, setPrompt] = useState("Sonnet 29 by William Shakespeare")
+  const START_TIME = 500
   const [text, setText] = useState('')
   const [wordCount, setWordCount] = useState(0)
   const [isTimeRunning, setIsTimeRunning] = useState(false)
   const [timeRemaining, setTimeRemaining] = useState(START_TIME)
   const inputEl = useRef(null)
+  const startButton = useRef(null)
+  const promptRef = useRef(null)
   const [pasteWarning, setPasteWarning] = useState(false)
 
   const startGame = () => {
+    setPrompt(promptGen.next().value)
+    setPromptGen(promptGen)
     setIsTimeRunning(true)
     inputEl.current.disabled = false
     setText('')
@@ -32,6 +39,8 @@ export default () => {
       setIsTimeRunning(false)
     }
   }
+
+  useEffect(() => startButton.current.focus(), [])
 
   useEffect(() => {
     const countWords = (text) => {
@@ -45,24 +54,29 @@ export default () => {
       }, 15)
     } else if (!isTimeRunning && timeRemaining < START_TIME) {
       setTimeout(() => {
-        setTimeRemaining(time => time + 1)
+        setTimeRemaining(time => time + 5)
       }, 2)
     } else if (timeRemaining === 0) {
       setIsTimeRunning(false)
       inputEl.current.onpaste = e => null
       countWords(inputEl.current.value)
+    } else {
+      startButton.current.focus()
     }
 
   }, [timeRemaining, isTimeRunning])
 
   const progressBarColor = (!isTimeRunning && timeRemaining < START_TIME) ? '' : 'is-info'
 
+
   return (
     <Section>
       <Container>
         <Columns isCentered>
-          <Column isSize="1/2" hasTextAlign="centered">
-            <Box hasTextAlign="centered"><q>But, soft! what light through yonder window breaks? It is the east, and Juliet is the sun.</q></Box>
+          <Column isSize="1/2">
+            <Box className="is-size-5">
+              {prompt}
+            </Box>
             <Field>
               <textarea
                 className="textarea"
@@ -79,7 +93,7 @@ export default () => {
               max={START_TIME}
               style={{ transform: `rotate(0.5turn)` }} />
             <Section style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-              <Button isSize="large" isColor="success" onClick={startGame} disabled={isTimeRunning || timeRemaining < START_TIME}>START</Button>
+              <button className="button is-large is-success" ref={startButton} onClick={startGame} disabled={isTimeRunning || timeRemaining < START_TIME}>START</button>
               <span className="is-size-4">WORDS: {wordCount}</span>
             </Section>
           </Column>
---
+++ app/javascript/helpers.js
@@ -0,0 +1,41 @@
+
+import React from "react"
+
+const sonnet29 = `When, in disgrace with fortune and men’s eyes,
+I all alone beweep my outcast state,
+And trouble deaf heaven with my bootless cries,
+And look upon myself and curse my fate,
+Wishing me like to one more rich in hope,
+Featured like him, like him with friends possessed,
+Desiring this man’s art and that man’s scope,
+With what I most enjoy contented least;
+Yet in these thoughts myself almost despising,
+Haply I think on thee, and then my state,
+(Like to the lark at break of day arising
+From sullen earth) sings hymns at heaven’s gate;
+For thy sweet love remembered such wealth brings
+That then I scorn to change my state with kings.`
+
+let sonnetLines = sonnet29.split(/\n/).map(line => line.trim())
+
+const couplets = []
+for (let i = 0; i < sonnetLines.length; i += 2) {
+  couplets.push(sonnetLines.slice(i, i + 2).join('\n'))
+}
+
+const prompts = couplets.map(couplet => {
+  const [line1, line2] = couplet.split(/\n/)
+  return <p>{line1}<br />{line2}</p>
+})
+
+export function* testPrompt() {
+  let i = 0
+  while (true) {
+    yield prompts[i]
+    if (i === prompts.length - 1) {
+      i = 0
+    } else {
+      i += 1
+    }
+  }
+}
