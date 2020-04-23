beba5d1f72d2ac6170be46ba5f2ae3de21a81cbe
Forrest Hayes <forresthayes@pm.me>
2020-04-22 22:31:55 -0500

    Make Header title a home link

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 import 'bulma/css/bulma.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFlagCheckered } from '@fortawesome/free-solid-svg-icons'
@@ -10,10 +11,12 @@ export default () => (
     <Container>
       <Columns isCentered>
         <Column isSize="1/2">
-          <Title hasTextColor="white" isSize={1}>
-            Speed Typer
+          <Link to="/">
+            <Title hasTextColor="white" isSize={1}>
+              Speed Typer
             <span style={{ marginLeft: "1rem" }}><FontAwesomeIcon icon={faFlagCheckered} /></span>
-          </Title>
+            </Title>
+          </Link>
           <Subtitle hasTextColor="white">How many words can you type before time runs out...</Subtitle>
         </Column>
       </Columns>
