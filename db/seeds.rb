b33542f7f0f456ffc164a1727db8472551b9836d
Forrest Hayes <forresthayes@pm.me>
2020-04-29 15:14:24 -0500

    Change approach to re-seeding to work around heroku guards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+
+Score.destroy_all
+
 names = %w(Amir James Mary Jennifer John Lizzie Anna Thomas Lisa Daniel)
 
 names.each do |name|
